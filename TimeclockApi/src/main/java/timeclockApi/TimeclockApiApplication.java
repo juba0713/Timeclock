e8fbdb777e50079319d8d69380deb5a054c51610
JBasas <jbasas@fullscale.ph>
2025-03-20 14:23:51 +0800

    Initial Files

---
+++ TimeclockApi/src/main/java/timeclockApi/TimeclockApiApplication.java
@@ -0,0 +1,13 @@
+package timeclockApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeclockApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeclockApiApplication.class, args);
+	}
+
+}
